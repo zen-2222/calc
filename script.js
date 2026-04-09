a7bb25daa071a1d2622fa2c6980fc6ad7ebe49a0
M <233329682+zen-2222@users.noreply.github.com>
2026-04-09 20:22:34 +0300

    finished functionality

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
 const btns = document.querySelector(".buttons")
 const text = document.querySelector('.text')
 
+function compute(equation){
+//multiplication, power , divison & mod loop
+let i
+for(i=0;i<equation.length;i++){
+    let first=parseFloat(equation[i-1]),second=parseFloat(equation[i+1])
+    switch(equation[i]){
+        case '%':{
+            let result=first % second
+            equation.splice(i-1,3,result)
+            i--
+            break;
+        }
+        case '*':{
+            let result=first * second
+            equation.splice(i-1,3,result)
+            i--
+            break;
+        }
+        case '^':{
+            let result=first ** second
+            equation.splice(i-1,3,result)
+            i--
+            break;
+        }
+        case '÷':{
+            let result=first / second
+            equation.splice(i-1,3,result)
+            i--
+            break;
+        }}
+}
+//add & sub loop
+for(i=0;i<equation.length;i++){
+    let first=parseFloat(equation[i-1]),second=parseFloat(equation[i+1])
+    switch(equation[i]){
+        case '−':{
+            let result=first - second
+            equation.splice(i-1,3,result)
+            i--
+            
+            break;
+        }
+        case '+':{
+            let result=first + second
+            equation.splice(i-1,3,result)
+            i--
+            break;
+        }
+    }
+}
+return equation
+
+}
+
+
 btns.addEventListener('click',(e)=>{ 
 if(e.target.classList.value == "btn"){
     e.preventDefault()
@@ -13,12 +68,15 @@ if(e.target.classList.value == "btn"){
             break
         }
         case '=':{
-            let equation=text.value.split(/([+|-|÷|*|^|%])/)
-            
+            let equation=text.value.split(/([+|−|÷|*|^|%])/)
+            if(equation[0] == "" && (equation[1]=='+' || equation[1]=='−')) equation[0]='0'
+
             if(equation.includes('')){alert('ERROR')
                 text.value=""
+            return
             }
-
+            text.value=compute(equation)
+            
             break
         }
         default:{ 
