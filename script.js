a8f076142405044ebd3baa748dd3d2dd140e41fb
M <233329682+zen-2222@users.noreply.github.com>
2026-04-09 18:57:29 +0300

    add interactivity

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-let nums=0
-for(let i=0;i<9;i++){
+const btns = document.querySelector(".buttons")
+const text = document.querySelector('.text')
 
+btns.addEventListener('click',(e)=>{ 
+if(e.target.classList.value == "btn"){
+    e.preventDefault()
+    let operation= e.target.innerText
+    switch(operation){
+        case 'C':{text.value=''
+            break
+        }
+        case `←`:{text.value=text.value.slice(0,-1)
+            break
+        }
+        case '=':{
+            let equation=text.value.split(/([+|-|÷|*|^|%])/)
+            
+            if(equation.includes('')){alert('ERROR')
+                text.value=""
+            }
 
+            break
+        }
+        default:{ 
+            text.value=text.value.concat(e.target.innerText)
+            
+            break
+        }
+        
+    }
 
 }
+
+})
